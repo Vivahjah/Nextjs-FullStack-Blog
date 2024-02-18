f5dacba248181271d02b62dac89e0e3f9e6929bc
Mengwa Ugo <mengwa44@yahoo.com>
2024-02-18 02:51:51 +0100

    added footer an nav component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import Links from "./Links"
+import style from "./navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="">Logo</div>
+        <div className=""><Links /></div>
+
+    </div>
+  )
+}
+
+export default Navbar
