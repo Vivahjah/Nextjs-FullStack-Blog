1ea4daaa7a88f2954abb5fc589a2f8b724a6c0b3
Mengwa Ugo <mengwa44@yahoo.com>
2024-02-29 19:32:24 +0100

    added singlePost

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,10 +1,37 @@
+import PostUser from "@/components/postUser/postUser";
+import styles from "./singlePost.module.css"
+import Image from "next/image";
+import { Suspense } from "react";
 
 
 const SinglePostPage = () => {
-    return (
-      <div>SinglePostPage</div>
-    )
-  }
+  return (
+    <div className={styles.container}>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(4, 16)}
+            </span>
+          </div>
+        </div>
+        <div className={styles.content}>{post.desc}</div>
+      </div>
+    </div>
+  );
+};
   
   export default SinglePostPage
   
